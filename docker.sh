70557ee74e4af9ef64f41a57e39dfc44929967ea
rrosaguado <45945583+rrosaguado@users.noreply.github.com>
2018-12-17 18:36:41 +0100

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-apt update
-apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-apt update
-apt-cache policy docker-ce
-apt install docker-ce
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce
